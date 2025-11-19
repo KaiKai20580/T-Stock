abc7d8c6076b52cb5fb5472350fac5e95938cf51
Kai Kai <alvinlyk20580@gmail.com>
2025-11-20 00:11:56 +0800

    main page

--- T-Stock/wwwroot/js/site.js
+++ T-Stock/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let btn = document.querySelector('#btn');
+let sidebar = document.querySelector('.sidebar');
 
-// Write your JavaScript code.
+btn.onclick = function () {
+    sidebar.classList.toggle('active');
+}
