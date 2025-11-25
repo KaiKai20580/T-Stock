809ec4c8f34daf76461ce97790e9e83968881958
Kah Wooi <koaykahwooi@gmail.com>
2025-11-25 21:12:15 +0800

    SupplierPO

--- T-Stock/obj/Debug/net9.0/T-Stock.AssemblyInfo.cs
+++ T-Stock/obj/Debug/net9.0/T-Stock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("T-Stock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb4ca8d23dbcf9540bb55169ba926a4b90c20302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9991ab79b4b29ccd655e170863fa3d7a2eaea106")]
 [assembly: System.Reflection.AssemblyProductAttribute("T-Stock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("T-Stock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
