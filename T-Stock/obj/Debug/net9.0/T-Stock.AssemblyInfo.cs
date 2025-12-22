4b320d59567f226711fab257062d5b7383c9f625
Kah Wooi <koaykahwooi@gmail.com>
2025-12-22 15:51:48 +0800

    fix search bar

--- T-Stock/obj/Debug/net9.0/T-Stock.AssemblyInfo.cs
+++ T-Stock/obj/Debug/net9.0/T-Stock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("T-Stock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2005f3a5a3bb6a0d0c66c32596005ce7311c016")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b4c2ad4e931bfa4f3899e52cdd5ea949c900e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("T-Stock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("T-Stock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
